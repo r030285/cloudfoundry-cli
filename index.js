4e931a4d86a7b64d6435cc08f31a2b744fa8a397
Hage Yaapa <captain@hacksparrow.com>
2017-04-28 04:06:34 +0530

    push app, delete app

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict'
 
 var path = require('path')
+var pushApp = require(path.join(__dirname, 'lib', 'push-app'))
 var getApps = require(path.join(__dirname, 'lib', 'get-apps'))
+var deleteApp = require(path.join(__dirname, 'lib', 'delete-app'))
 var getServices = require(path.join(__dirname, 'lib', 'get-services'))
 var createService = require(path.join(__dirname, 'lib', 'create-service'))
 var deleteService = require(path.join(__dirname, 'lib', 'delete-service'))
@@ -9,8 +11,14 @@ var bindService = require(path.join(__dirname, 'lib', 'bind-service'))
 
 var cloudFoundry = {}
 
+cloudFoundry.pushApp = pushApp()
+cloudFoundry.pushAppSync = pushApp(true)
+
 cloudFoundry.getApps = getApps()
 cloudFoundry.getAppsSync = getApps(true)
+
+cloudFoundry.deleteApp = deleteApp()
+cloudFoundry.deleteAppSync = deleteApp(true)
 
 cloudFoundry.getServices = getServices()
 cloudFoundry.getServicesSync = getServices(true)
